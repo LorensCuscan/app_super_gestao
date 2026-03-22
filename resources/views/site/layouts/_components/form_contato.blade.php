2d3bb2c54369288a47e3e9d6bb94492c71da3a31
Lorens <lorenscuscan@gmail.com>
2026-03-22 17:47:09 +0000

    applying corrections in blade

--- resources/views/site/layouts/_components/form_contato.blade.php
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -1,46 +1,36 @@
 {{ $slot }}
 
-{{ $classe }}
-
-
-
-
 <form action="{{ route('site.contato.salvar') }}" method="post">
     @csrf
+    
     <input name="nome" value="{{ old('nome') }}" type="text" placeholder="Nome" class="{{ $classe }}">
     @if ($errors->has('nome'))
-        {{ $errors->first('nome') }}
+        <span style="color: red;">{{ $errors->first('nome') }}</span>
     @endif
     <br>
+
     <input name="telefone" value="{{ old('telefone') }}" placeholder="Telefone" class="{{ $classe }}">
-    {{ $errors->has('telefone') ? $errors->first('telefone') : ''}}
+    <span style="color: red;">{{ $errors->has('telefone') ? $errors->first('telefone') : ''}}</span>
     <br>
+
     <input name="email" value="{{ old('email') }}" placeholder="E-mail" class="{{ $classe }}">
-    {{ $errors->has('email') ? $errors->first('email') : ''}}
+    <span style="color: red;">{{ $errors->has('email') ? $errors->first('email') : ''}}</span>
     <br>
-   
-   
 
     <select name="motivo_contato_id" class="{{ $classe }}">
         <option value="">Qual o motivo do contato?</option>
         @foreach ($motivo_contato as $motivo)
-           <option value="{{ $motivo->id }}" @selected(old('motivo_contato_id') == $motivo->id)>
-    {{ $motivo->motivo_contato }}
-</option>
+            <option value="{{ $motivo->id }}" @selected(old('motivo_contato_id') == $motivo->id)>
+                {{ $motivo->motivo_contato }}
+            </option>
         @endforeach
     </select>
-    {{ $errors->has('motivo_contato_id') ? $errors->first('motivo_contato_id') : ''}}
+    <span style="color: red;">{{ $errors->has('motivo_contato_id') ? $errors->first('motivo_contato_id') : ''}}</span>
+    <br>
 
-       
-    </select>
+    <textarea name="mensagem" class="{{ $classe }}">{{ (old('mensagem') != '') ? old('mensagem') : 'Preencha aqui sua mensagem' }}</textarea>
+    <span style="color: red;">{{ $errors->has('mensagem') ? $errors->first('mensagem') : ''}}</span>
     <br>
-    <textarea name="mensagem" class="{{ $classe }}">{{ (old('mensagem') != '') ? old('mensagem') :  'Preencha aqui sua mensagem'}}</textarea>
-    {{ $errors->has('mensagem') ? $errors->first('mensagem') : ''}}
-    <br>
+
     <button type="submit" class="{{ $classe }}">ENVIAR</button>
-
-
-    
 </form>
-
-
